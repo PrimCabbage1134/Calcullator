561232067c3850acaf37297ce88dc840211a8bcb
PrimCabbage1134 <79534007+PrimCabbage1134@users.noreply.github.com>
2021-02-23 23:03:49 +0530

    Add files via upload

---
+++ Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,48 @@
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    default:
+        return 0.0;
+    }
+}
+
+
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
+    cout << "Calculator made by PrimCabbage1134" << endl << endl;
+    //Feel free to play with me on xbox my gametag is PrimCabbage1134. I am typing this in the comment because it should not effect the code
+    //
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+        << endl;
+
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        result = c.Calculate(x, oper, y);
+        cout << "Result is: " << result << endl;
+    }
+
+    return 0;
+}
